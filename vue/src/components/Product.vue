606050d2a4f27dfedc87c3224ea18b70e16bd6ec
sHKamil <shootekb@gmail.com>
2024-02-05 17:01:50 +0100

    Pop product window

--- vue/src/components/Product.vue
+++ vue/src/components/Product.vue
@@ -3,6 +3,7 @@
     import type { PropType } from 'vue';
     import { GetBackendUrl } from '../utils/GetBackendUrl.js';
     import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+    import ProductWindow from './ProductWindow.vue';
 
     const props = defineProps({
         product: {
@@ -17,31 +18,20 @@
 </script>
 
 <template>
-    <!-- <div>
-        <div>
-            <img :src="product.image" width="250">
-        </div>
-        <div>
-            <p>Model: {{ product.name }}</p>
-            <p>Type: {{ product.type }}</p>
-            <p>Frame size: {{ product.size }} inches</p>
-            <p>Color: {{ product.color }}</p>
-            <p>Wheel size: {{ product.wheel_size }} inches</p>
-            <p>Price: ${{ product.price }}</p>
-        </div>
-    </div> -->
-    <Card>
-        <CardHeader>
-            <CardTitle>Model: {{ product.name }}</CardTitle>
-            <CardDescription>Type: {{ product.type }}</CardDescription>
-        </CardHeader>
-        <CardContent>
-            <img :src="imageUrl" class="w-full max-h-72 object-cover overflow-hidden">
-        </CardContent>
-        <CardFooter>
-            Price: ${{ product.price }}
-        </CardFooter>
-    </Card>
+    <ProductWindow :product="product" :imageUrl="imageUrl">
+        <Card>
+            <CardHeader>
+                <CardTitle>Model: {{ product.name }}</CardTitle>
+                <CardDescription>Type: {{ product.type }}</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <img :src="imageUrl" class="w-full max-h-72 object-cover overflow-hidden" draggable="false">
+            </CardContent>
+            <CardFooter class="mt-3">
+                Price: ${{ product.price }}
+            </CardFooter>
+        </Card>
+    </ProductWindow>
 </template>
 
 <style scoped>
---
+++ vue/src/components/ProductWindow.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import type { ProductType } from '@/utils/types/ProductType.js';
+import type { PropType } from 'vue';
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from '@/components/ui/alert-dialog'
+const props = defineProps({
+        product: {
+            type: Object as PropType<ProductType>,
+            required: true
+        },
+        imageUrl: String
+    });
+
+</script>
+
+<template>
+  <AlertDialog>
+    <AlertDialogTrigger><slot></slot></AlertDialogTrigger>
+    <AlertDialogContent class="p-0 gap-0">
+      <AlertDialogHeader>
+        <AlertDialogTitle>
+          <img :src="imageUrl" class="w-full max-h-72 object-cover overflow-hidden rounded-t-md" draggable="false">
+        </AlertDialogTitle>
+        <AlertDialogDescription class="p-3">
+          <p class="text-lg">Model: {{ product.name }}</p>
+          <p>Color: {{ product.color }}</p>
+          <p>Type: {{ product.type }}</p>
+          <p>Frame size: {{ product.size }}</p>
+          <p>Wheel size: {{ product.wheel_size }}</p>
+        </AlertDialogDescription>
+      </AlertDialogHeader>
+      <AlertDialogFooter class="p-3">
+        <AlertDialogCancel>Cancel</AlertDialogCancel>
+        <AlertDialogAction>Reserve</AlertDialogAction>
+      </AlertDialogFooter>
+    </AlertDialogContent>
+  </AlertDialog>
+</template>
